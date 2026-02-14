7eff18832fe9f947691a365dc8ce22a4771b5cd8
oumaima amlou <amlou.oumaima@gmail.com>
2026-02-14 21:00:39 +0100

    add 3 things babeee

---
+++ pieces.js
@@ -0,0 +1,42 @@
+ console.log("hey beauty  ");
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+
+console.log(pieces);
+for (let index = 0; index < pieces.length; index++) {
+     const article = pieces[index];
+
+    const sectionFiches = document.querySelector(".fiches");
+    const ArticleBalise = document.createElement("article");
+
+    const imageElement =document.createElement("img")
+    imageElement.src=article.image;
+
+    const nomArticle =document.createElement("h2")
+    nomArticle.innerText=article.nom;
+
+    const prixElement =document.createElement("p")
+    prixElement.innerText=`prix : ${article.prix}  ${article.prix <20 ? "€" : "€€€"} `;
+
+    const categorieElement =document.createElement("p")
+    categorieElement.innerText=article.categorie ?? "aucune catégorie";
+
+    const descriptionElement =document.createElement("p")
+    descriptionElement.innerText=article.description ?? "Pas de description pour le moment.";
+
+    const disponElement =document.createElement("p")
+    disponElement.innerText=article.disponibilite ? "En stock" : "Rupture de stock";
+    ArticleBalise.appendChild(imageElement);
+    ArticleBalise.appendChild(nomArticle);
+    ArticleBalise.appendChild(prixElement);
+    ArticleBalise.appendChild(categorieElement);
+    ArticleBalise.appendChild(descriptionElement);
+    ArticleBalise.appendChild(disponElement);
+
+    sectionFiches.appendChild(ArticleBalise);
+
+}
+
+
+
